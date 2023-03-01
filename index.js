c4e5e32ba09ef6035bf457abdaa803756e93be5a
Nikita <nikitostokarev22@gmail.com>
2023-03-01 18:42:32 +0300

    add listeners

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const cols = document.querySelectorAll('.col');
 
+document.addEventListener('keydown', e => {
+    e.preventDefault()
+    if (e.code.toLowerCase() === 'space') {
+        setRandomColors()
+    }
+})
+
+document.addEventListener('click', e => {
+    const type = e.target.dataset.type
+
+    if (type === 'lock') {
+        const node = e.target.tagName.toLowerCase() === 'i'
+            ? e.target
+            : e.target.children[0]
+        node.classList.toggle('fa-lock-open')
+        node.classList.toggle('fa-lock')
+    }
+})
 // Функция для создания рандомного цвета
 // function generateRandomColor() {
 //     const hexCodes = '0123456789ABCDEF'
@@ -12,11 +30,19 @@ const cols = document.querySelectorAll('.col');
 
 function setRandomColors() {
     cols.forEach(col => {
+        const isLocked = col.querySelector('i').classList.contains('fa-lock')
         const text = col.querySelector('h2')
+        const button = col.querySelector('button')
         const color = chroma.random()
+
+        if (isLocked) {
+            return
+        }
+
         text.textContent = color
         col.style.background = color
         setTextColor(text, color)
+        setTextColor(button, color)
     })
 }
 
