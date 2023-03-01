8cf50aeaf22cf0dca7949e5458ac42be824c6b2b
Nikita <nikitostokarev22@gmail.com>
2023-03-01 19:08:08 +0300

    add function copyToClipboard, add hash

--- index.js
+++ index.js
@@ -16,8 +16,64 @@ document.addEventListener('click', e => {
             : e.target.children[0]
         node.classList.toggle('fa-lock-open')
         node.classList.toggle('fa-lock')
+    } else if (type === 'copy') {
+        console.log(e.target)
+        copyToClickboard(e.target.textContent)
     }
 })
+function copyToClickboard(text) {
+    return navigator.clipboard.writeText(text)
+}
+function setRandomColors(isInitial) {
+    const colors = isInitial ? getColorsFromHash() : []
+    cols.forEach((col, index) => {
+        const isLocked = col.querySelector('i').classList.contains('fa-lock')
+        const text = col.querySelector('h2')
+        const button = col.querySelector('button')
+
+        if (isLocked) {
+            colors.push(text.textContent)
+            return
+        }
+        const color = isInitial
+            ? colors[index]
+                ? colors[index]
+                : chroma.random()
+            : chroma.random()
+
+        if (!isInitial) {
+            colors.push(color)
+        }
+
+        text.textContent = color
+        col.style.background = color
+
+        setTextColor(text, color)
+        setTextColor(button, color)
+    })
+    updateColorsHash(colors)
+}
+
+function setTextColor(text, color) {
+    const luminance = chroma(color).luminance()
+    text.style.color = luminance > 0.5 ? 'black' : 'white'
+}
+
+function updateColorsHash(colors = []) {
+    document.location.hash = colors.map(el => {
+        return el.toString().substring(1)
+    }).join('-')
+}
+function getColorsFromHash() {
+    if (document.location.hash.length > 1) {
+        return document.location.hash.substring(1).split('-').map(color => {
+            return '#' + color
+        })
+    }
+    return []
+}
+setRandomColors(true)
+
 // Функция для создания рандомного цвета
 // function generateRandomColor() {
 //     const hexCodes = '0123456789ABCDEF'
@@ -27,28 +83,3 @@ document.addEventListener('click', e => {
 //     }
 //     return '#' + color
 // }
-
-function setRandomColors() {
-    cols.forEach(col => {
-        const isLocked = col.querySelector('i').classList.contains('fa-lock')
-        const text = col.querySelector('h2')
-        const button = col.querySelector('button')
-        const color = chroma.random()
-
-        if (isLocked) {
-            return
-        }
-
-        text.textContent = color
-        col.style.background = color
-        setTextColor(text, color)
-        setTextColor(button, color)
-    })
-}
-
-function setTextColor(text, color) {
-    const luminance = chroma(color).luminance()
-    text.style.color = luminance > 0.5 ? 'black' : 'white'
-}
-
-setRandomColors()
